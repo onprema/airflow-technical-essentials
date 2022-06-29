e9488a67ec5a9201e69f12e8c83175e5da09947b
Lee Gaines <lgaines@autodesk.com>
2022-06-29 09:15:54 -0400

    update dags

--- README.md
+++ README.md
@@ -18,7 +18,14 @@ A basic DAG that introduces the concepts of **tasks**, **operators**, **dependen
 ## dags/weather_checker.py
 A more robust DAG that uses **variable**, **connection**, **xcom**, **SimpleHTTPOperator**, and **task context**.
 
-## Accessing the metastore
+## dags/query_dag.py
+A dag that queries the Airflow metastore using the `PostgresOperator`.
+
+## Accessing the metastore container
 ```
 docker exec -it $POSTGRES_CONTAINER psql -h postgres -U postgres
 ```
+
+## Notes
+- The **logical date**, formerly known as **execution date**, is the start of the _data interval_, not when the DAG is actually executed.
+- Environment variables override `airflow.cfg` (See `.env` for examples)
---
+++ dags/query_dag.py
@@ -0,0 +1,39 @@
+"""
+Try to query the airflow metastore in our DAG
+"""
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.hooks.postgres_hook import PostgresHook
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 3,
+    'retry_delay': timedelta(seconds=30),
+    'do_xcom_push': False
+}
+
+dag = DAG(
+    dag_id='query_dag',
+    start_date=datetime(2020, 1, 1),
+    schedule_interval='0 0 * * *', # midnight everyday
+    default_args=default_args,
+    catchup=False
+)
+
+def get_query_results():
+    sql = 'select * from variable'
+    pg_hook = PostgresHook(postgres_conn_id='airflow_conn')
+    conn = pg_hook.get_conn()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
+
+with dag:
+    run_query = PythonOperator(
+        task_id='run_query',
+        python_callable=get_query_results 
+    )
+    run_query
